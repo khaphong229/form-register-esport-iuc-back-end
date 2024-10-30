0430edd5862485e28245bfaea82f33b6c43ef0b4
khaphong229 <khaphong217@gmail.com>
2024-10-30 20:38:27 +0700

    donee

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,32 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import eslintPluginPrettier from "eslint-plugin-prettier";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   {
     plugins: {
-      prettier: eslintPluginPrettier,
+      prettier: eslintPluginPrettier
     },
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "prettier/prettier": [
-        "warn",
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'prettier/prettier': [
+        'warn',
         {
-          arrowParens: "always",
+          arrowParens: 'always',
           semi: false,
-          trailingComma: "none",
+          trailingComma: 'none',
           tabWidth: 2,
-          endOfLine: "auto",
+          endOfLine: 'auto',
           useTabs: false,
           singleQuote: true,
           printWidth: 120,
-          jsxSingleQuote: true,
-        },
-      ],
+          jsxSingleQuote: true
+        }
+      ]
     },
-    ignores: ["**/node_modules/", "**/dist/"],
-  },
-];
+    ignores: ['**/node_modules/', '**/dist/']
+  }
+]
